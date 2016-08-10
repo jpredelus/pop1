b0885758a3f1d80c5584a3e60d5b2038522639e0
Yuri Blaise <blaiseyuri@ufl.edu>
2016-08-10 08:46:46 -0400

    Added animator helper function for ng-class. Added documentation for home controller.

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -3,28 +3,80 @@
 
 class HomeController {
     constructor($scope, $timeout) {
-        $scope.firstLoad = true;
-        
-        // used to determine whether an animation should be applied
-        this.scrollCheck = (page, direction = null) => {
-            if (!direction) {
-                return page === $scope.page || $scope.firstLoad;
-            }
-            else {
-                return (page === $scope.page && direction === $scope.dir) || $scope.firstLoad;
-            }
-        };
+        $scope.firstLoad = true; // used to determine if afterLoad callback has been called
 
-        const afterLoadFn = (anchorLink, index, slideAnchor, slideIndex) => {
+        /**
+         * Callback that is fired after slide loads
+         * Updates the scope index to the current section's index
+         * also updates the direction of the most recent transition. 
+         * 
+         * @param  {[string]} anchorLink: anchorLink corresponding to the section.
+         * @param  {[int]} index: index of the section. Starting from 1.
+         * @param  {[string]} slideAnchor: anchor corresponding to the slide (in case there is)
+         * @param  {[type]} slideIndex: index of the slide. Starting from 1. (the default slide doesn't count as slide, but as a section)
+         */
+        function afterLoadFn  (anchorLink, index, slideAnchor, slideIndex)  {
             $timeout( () => {
-                const lastidx = $scope.page;
-                $scope.page = index;
-                $scope.dir= lastidx > $scope.page && angular.isNumber(lastidx) ?  'up' : 'down';
+                const lastidx = $scope.idx;
+                $scope.idx = index;
+                $scope.dir= lastidx > $scope.idx && angular.isNumber(lastidx) ?  'up' : 'down';
                 $scope.firstLoad = false;
             });
-        };
+        }
 
-        this.options = { navigation : true, afterLoad : afterLoadFn};
+        /**
+         * Checks whether params match the current section's index and the direction 
+         * of the last transition. If the afterLoadfn hasn't been called then it will 
+         * return true regardless of params.
+         * 
+         * @param  {[int]} idx: The index of the section
+         * @param  {[string]} direction: direction of the last section transition can be "up" or "down",
+         * If null there is no check.
+         * 
+         * @return {[boolean]} true if params match or if afterLoadfn has not been called atleast once.
+         */
+        function sectionCheck (idx, direction = null) {
+            if (!direction) {
+                return idx === $scope.idx || $scope.firstLoad;
+            }
+            else {
+                return (idx === $scope.idx && direction === $scope.dir) || $scope.firstLoad;
+            }
+        }
+
+        /**
+         * Creates an object for ng-class to use to determine whether to add
+         * a class to an element or not. This is used for css animations.
+         * 
+         * @param  {[type]} idx: Index of what the current section index should be for the class to be added
+         * 
+         * @param  {[string]} up: The name of the class that should be added if the transition direction is up.
+         * This will also be added when going down if down is null
+         * 
+         * @param  {[type]} down: same as up except for the down direction, if null direction is ignored and 
+         * up class is added regardless of direction.
+         * 
+         * 
+         * @param  {String} exit: the name of the class that should be added after exiting a sextion. The default is fadeOut
+         * since the animation will not be seen.
+         * 
+         * @return {[object]} A dictionary of the class names and booleans. If a class's value is true 
+         * the class is added to the element
+         */
+        function animator (idx, up, down = null, exit = 'fadeOut') {
+            if (down) {
+                return {
+                    [up]: sectionCheck(idx, 'up'),
+                    [down]: sectionCheck(idx, 'down'),
+                    [exit]: !sectionCheck(idx)
+                };
+            } else {
+                return {[up]: sectionCheck(idx), [exit]: !sectionCheck(idx)};
+            }
+        }
+
+        this.animator = animator; // so that the view can access the animator function
+        this.options = { navigation : true, afterLoad : afterLoadFn}; //options for fullpagejs module
     }
 }
 
