72b5d1217ec98927504dce45f11c367f5d6d8e48
Yuri Blaise <blaiseyuri@ufl.edu>
2017-05-09 10:42:35 -0400

    Added custom sidenav to store, downgraded to angular 1.5.5

--- client/app/store/components/store.controller.js
+++ client/app/store/components/store.controller.js
@@ -3,9 +3,25 @@
 
 // Parent Component for the store
 class StoreController {
-  constructor() {
+  constructor($mdSidenav, $window, $animate) {
     this.message = 'Hello';
+    this.open = false;
     this.images = ['blacksuit.jpeg', 'bwsuit.jpeg', 'blackshoes.jpeg','graysuit.jpeg','redsuit.jpeg'];
+
+    // Function animates sidenav and itemlist so that they shrink and grow smoothly
+    this.toggle = () => {
+        const el = angular.element('item-list');
+        const el2 = angular.element('md-sidenav');
+
+        if(!this.open) {
+            $animate.addClass(el, 'md-opened-custom');
+            $animate.addClass(el2, 'md-opened-custom');
+        } else{
+            $animate.removeClass(el, 'md-opened-custom');
+            $animate.removeClass(el2, 'md-opened-custom');
+        }
+        this.open = !this.open;
+    };
   }
 }
 
