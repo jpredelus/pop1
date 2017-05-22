610e616c5bd51f5aa513211ebb761124f06a2543
Yuri Blaise <blaiseyuri@ufl.edu>
2017-05-21 21:51:26 -0400

    Added shipping section of profile

--- client/app/account/profile/account.controller.js
+++ client/app/account/profile/account.controller.js
@@ -9,6 +9,10 @@ class AccountController {
     this.orders = MockStore.createOrders(4,3);
     this.selected = 'Profile';
 
+     this.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    'WY').split(' ');
+
     this.select = (idx) => {
       const selTab = this.tabs[idx];
       this.selected = selTab.name;
