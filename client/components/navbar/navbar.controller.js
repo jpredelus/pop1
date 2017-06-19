fc580e658255b7f69763d3b4554a4f8e87859ecc
Yuri Blaise <blaiseyuri@ufl.edu>
2017-06-19 17:38:58 -0400

    Redesigned Navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,12 +4,13 @@ class NavbarController {
   //end-non-standard
 
   //start-non-standard
-  constructor(Auth, $location, $mdDialog) {
+  constructor(Auth, $location, $mdDialog, $state) {
     this.isPath = (path) => path === $location.path().replace('/','');
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
     this.getCurrentUser = Auth.getCurrentUser;
     this.getTheme = () => this.isPath('home') ? 'navdark' : 'navbar';
+    this.stateName = $state.current.name;
     this.login = (ev) => {
       $mdDialog.show({
         controller: 'LoginController',
@@ -18,7 +19,11 @@ class NavbarController {
         parent: angular.element(document.body),
         targetEvent: ev,
         clickOutsideToClose:true,
-        fullscreen: false
+        fullscreen: false,
+        onRemoving: (ev, promise) => {
+            console.log('closing');
+            this.stateName = $state.current.name;
+          }
       });
     };
 
